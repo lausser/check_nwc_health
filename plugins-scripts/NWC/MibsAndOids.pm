b2c1230766e70d11032857303fa76fb8baa26595
Gerhard Lausser <gerhard.lausser@consol.de>
2010-10-28 22:09:19 +0200

    separate code from data. oids get their own file

---
+++ plugins-scripts/NWC/MibsAndOids.pm
@@ -0,0 +1,58 @@
+$NWC::Device::mibs_and_oids = {
+  'MIB-II' => {
+      sysDescr => '1.3.6.1.2.1.1.',
+      #
+      ifTable => '1.3.6.1.2.1.2.2',
+      ifEntry => '1.3.6.1.2.1.2.2.1',
+      ifIndex => '1.3.6.1.2.1.2.2.1.1',
+      ifDescr => '1.3.6.1.2.1.2.2.1.2',
+      ifType => '1.3.6.1.2.1.2.2.1.3',
+      ifMtu => '1.3.6.1.2.1.2.2.1.4',
+      ifSpeed => '1.3.6.1.2.1.2.2.1.5',
+      ifPhysAddress => '1.3.6.1.2.1.2.2.1.6',
+      ifAdminStatus => '1.3.6.1.2.1.2.2.1.7',
+      ifOperStatus => '1.3.6.1.2.1.2.2.1.8',
+      ifLastChange => '1.3.6.1.2.1.2.2.1.9',
+      ifInOctets => '1.3.6.1.2.1.2.2.1.10',
+      ifInUcastPkts => '1.3.6.1.2.1.2.2.1.11',
+      ifInNUcastPkts => '1.3.6.1.2.1.2.2.1.12',
+      ifInDiscards => '1.3.6.1.2.1.2.2.1.13',
+      ifInErrors => '1.3.6.1.2.1.2.2.1.14',
+      ifInUnknownProtos => '1.3.6.1.2.1.2.2.1.15',
+      ifOutOctets => '1.3.6.1.2.1.2.2.1.16',
+      ifOutUcastPkts => '1.3.6.1.2.1.2.2.1.17',
+      ifOutNUcastPkts => '1.3.6.1.2.1.2.2.1.18',
+      ifOutDiscards => '1.3.6.1.2.1.2.2.1.19',
+      ifOutErrors => '1.3.6.1.2.1.2.2.1.20',
+      ifOutQLen => '1.3.6.1.2.1.2.2.1.21',
+      ifSpecific => '1.3.6.1.2.1.2.2.1.22',
+      ifAdminStatusValue => {
+          1 => 'up',
+          2 => 'down',
+          3 => 'testing',
+      },
+      ifOperStatusValue => {
+          1 => 'up',
+          2 => 'down',
+          3 => 'testing',
+          4 => 'unknown',
+          5 => 'dormant',
+          6 => 'notPresent',
+          7 => 'lowerLayerDown',
+      },
+      # INDEX { ifIndex }
+  },
+  'CISCO-MEMORY-POOL-MIB' => {
+      ciscoMemoryPoolTable => '1.3.6.1.4.1.9.9.48.1.1',
+      ciscoMemoryPoolEntry => '1.3.6.1.4.1.9.9.48.1.1.1',
+      ciscoMemoryPoolType => '1.3.6.1.4.1.9.9.48.1.1.1.1',
+      ciscoMemoryPoolName => '1.3.6.1.4.1.9.9.48.1.1.1.2',
+      ciscoMemoryPoolAlternate => '1.3.6.1.4.1.9.9.48.1.1.1.3',
+      ciscoMemoryPoolValid => '1.3.6.1.4.1.9.9.48.1.1.1.4',
+      ciscoMemoryPoolUsed => '1.3.6.1.4.1.9.9.48.1.1.1.5',
+      ciscoMemoryPoolFree => '1.3.6.1.4.1.9.9.48.1.1.1.6',
+      ciscoMemoryPoolLargestFree => '1.3.6.1.4.1.9.9.48.1.1.1.7',
+      # INDEX { ciscoMemoryPoolType }
+  },
+
+};
