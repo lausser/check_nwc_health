97100220459128714e7f51aa1b46065438a5661f
Gerhard Lausser <gerhard.lausser@consol.de>
2012-03-22 15:38:50 +0100

    add some missing files

---
+++ plugins-scripts/NWC/CiscoIOS/Component/CpuSubsystem.pm
@@ -0,0 +1,142 @@
+package NWC::CiscoIOS::Component::CpuSubsystem;
+our @ISA = qw(NWC::CiscoIOS);
+
+use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = {
+    cpus => [],
+    blacklisted => 0,
+    info => undef,
+    extendedinfo => undef,
+  };
+  bless $self, $class;
+  $self->init(%params);
+  return $self;
+}
+
+sub init {
+  my $self = shift;
+  my %params = @_;
+  my $type = 0;
+  foreach ($self->get_snmp_table_objects(
+      'CISCO-PROCESS-MIB', 'cpmCPUTotalTable')) {
+    $_->{cpmCPUTotalIndex} ||= $type++;
+    push(@{$self->{cpus}},
+        NWC::CiscoIOS::Component::CpuSubsystem::Cpu->new(%{$_}));
+  }
+  if (scalar(@{$self->{cpus}}) == 0) {
+    # maybe too old. i fake a cpu. be careful. this is a really bad hack
+    my $response = $self->get_request(
+        -varbindlist => [
+            $NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy1},
+            $NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy5},
+            $NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{busyPer},
+        ]
+    );
+    if (exists $response->{$NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy1}}) {
+      push(@{$self->{cpus}},
+          NWC::CiscoIOS::Component::CpuSubsystem::Cpu->new(
+              cpmCPUTotalPhysicalIndex => 0, #fake
+              cpmCPUTotalIndex => 0, #fake
+              cpmCPUTotal5sec => 0, #fake
+              cpmCPUTotal5secRev => 0, #fake
+              cpmCPUTotal1min => $response->{$NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy1}},
+              cpmCPUTotal1minRev => $response->{$NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy1}},
+              cpmCPUTotal5min => $response->{$NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy5}},
+              cpmCPUTotal5minRev => $response->{$NWC::Device::mibs_and_oids->{'OLD-CISCO-CPU-MIB'}->{avgBusy5}},
+              cpmCPUMonInterval => 0, #fake
+              cpmCPUTotalMonIntervalValue => 0, #fake
+              cpmCPUInterruptMonIntervalValue => 0, #fake
+      ));
+    }
+  }
+}
+
+sub check {
+  my $self = shift;
+  my $errorfound = 0;
+  $self->add_info('checking cpus');
+  $self->blacklist('ff', '');
+  if (scalar (@{$self->{cpus}}) == 0) {
+  } else {
+    foreach (@{$self->{cpus}}) {
+      $_->check();
+    }
+  }
+}
+
+
+sub dump {
+  my $self = shift;
+  foreach (@{$self->{cpus}}) {
+    $_->dump();
+  }
+}
+
+
+package NWC::CiscoIOS::Component::CpuSubsystem::Cpu;
+our @ISA = qw(NWC::CiscoIOS::Component::CpuSubsystem);
+
+use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = {
+    blacklisted => 0,
+    info => undef,
+    extendedinfo => undef,
+  };
+  foreach my $param (qw(cpmCPUTotalIndex cpmCPUTotalPhysicalIndex
+      cpmCPUTotal5sec cpmCPUTotal1min cpmCPUTotal5min
+      cpmCPUTotal5secRev cpmCPUTotal1minRev cpmCPUTotal5minRev
+      cpmCPUMonInterval cpmCPUTotalMonIntervalValue
+      cpmCPUInterruptMonIntervalValue)) {
+    $self->{$param} = $params{$param};
+  }
+  bless $self, $class;
+  $self->{usage} = $params{cpmCPUTotal5minRev};
+  if ($self->{cpmCPUTotalPhysicalIndex}) {
+    my $entPhysicalName = '1.3.6.1.2.1.47.1.1.1.1.7';
+    $self->{entPhysicalName} = $self->get_request(
+        -varbindlist => [$entPhysicalName.'.'.$self->{cpmCPUTotalPhysicalIndex}]
+    );
+    $self->{entPhysicalName} = $self->{entPhysicalName}->{$entPhysicalName.'.'.$self->{cpmCPUTotalPhysicalIndex}};
+  } else {
+    $self->{entPhysicalName} = $self->{cpmCPUTotalIndex};
+  }
+  return $self;
+}
+
+sub check {
+  my $self = shift;
+  $self->blacklist('c', $self->{cpmCPUTotalPhysicalIndex});
+  my $info = sprintf 'cpu %s usage (5 min avg.) is %.2f%%',
+      $self->{entPhysicalName}, $self->{usage};
+  $self->add_info($info);
+  $self->set_thresholds(warning => 80, critical => 90);
+  $self->add_message($self->check_thresholds($self->{usage}), $info);
+  $self->add_perfdata(
+      label => 'cpu_'.$self->{entPhysicalName}.'_usage',
+      value => $self->{usage},
+      uom => '%',
+      warning => $self->{warning},
+      critical => $self->{critical},
+  );
+}
+
+sub dump {
+  my $self = shift;
+  printf "[CPU_%s]\n", $self->{cpmCPUTotalPhysicalIndex};
+  foreach (qw(cpmCPUTotalIndex cpmCPUTotalPhysicalIndex cpmCPUTotal5sec cpmCPUTotal1min cpmCPUTotal5min cpmCPUTotal5secRev cpmCPUTotal1minRev cpmCPUTotal5minRev cpmCPUMonInterval cpmCPUTotalMonIntervalValue cpmCPUInterruptMonIntervalValue)) {
+    printf "%s: %s\n", $_, $self->{$_};
+  }
+  printf "info: %s\n", $self->{info};
+  printf "\n";
+}
+
---
+++ plugins-scripts/NWC/CiscoIOS/Component/EnvironmentalSubsystem.pm
@@ -0,0 +1,65 @@
+package NWC::CiscoIOS::Component::EnvironmentalSubsystem;
+our @ISA = qw(NWC::CiscoIOS);
+
+use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = {
+    runtime => $params{runtime},
+    rawdata => $params{rawdata},
+    method => $params{method},
+    condition => $params{condition},
+    status => $params{status},
+    fan_subsystem => undef,
+    temperature_subsystem => undef,
+    powersupply_subsystem => undef,
+    voltage_subsystem => undef,
+    blacklisted => 0,
+    info => undef,
+    extendedinfo => undef,
+  };
+  bless $self, $class;
+  $self->init(%params);
+  return $self;
+}
+
+sub init {
+  my $self = shift;
+  my %params = @_;
+  #
+  # 1.3.6.1.4.1.9.9.13.1.1.0 ciscoEnvMonPresent (irgendein typ of envmon)
+  # 
+  $self->{fan_subsystem} =
+      NWC::CiscoIOS::Component::FanSubsystem->new(%params);
+  $self->{temperature_subsystem} =
+      NWC::CiscoIOS::Component::TemperatureSubsystem->new(%params);
+  $self->{powersupply_subsystem} = 
+      NWC::CiscoIOS::Component::SupplySubsystem->new(%params);
+  $self->{voltage_subsystem} =
+      NWC::CiscoIOS::Component::VoltageSubsystem->new(%params);
+}
+
+sub check {
+  my $self = shift;
+  my $errorfound = 0;
+  $self->{fan_subsystem}->check();
+  $self->{temperature_subsystem}->check();
+  $self->{voltage_subsystem}->check();
+  $self->{powersupply_subsystem}->check();
+  if (! $self->check_messages()) {
+    $self->add_message(OK, "environmental hardware working fine");
+  }
+}
+
+sub dump {
+  my $self = shift;
+  $self->{fan_subsystem}->dump();
+  $self->{temperature_subsystem}->dump();
+  $self->{voltage_subsystem}->dump();
+  $self->{powersupply_subsystem}->dump();
+}
+
+1;
