eb41709fb760a730782f7afd4573f51b88009e7c
Gerhard Lausser <gerhard.lausser@consol.de>
2013-01-14 10:08:46 +0100

    make creation of a statefile atomic

--- plugins-scripts/NWC/Device.pm
+++ plugins-scripts/NWC/Device.pm
@@ -1404,9 +1404,10 @@ sub save_cache {
   my $statefile = lc sprintf "%s/%s_%s_%s-%s_%s_cache",
       $NWC::Device::statefilesdir, $self->opts->hostname,
       $self->opts->mode, $mib, $table, join('#', @{$key_attr});
-  open(STATE, ">$statefile");
+  open(STATE, ">".$statefile.".".$PID);
   printf STATE Data::Dumper::Dumper($self->{$cache});
   close STATE;
+  rename $statefile.".".$PID, $statefile;
   $self->debug(sprintf "saved %s to %s",
       Data::Dumper::Dumper($self->{$cache}), $statefile);
 }
