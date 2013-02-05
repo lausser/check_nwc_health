e863dd9410e7c7c68d6105c9a213486557ab07d7
Gerhard Lausser <gerhard.lausser@consol.de>
2013-02-05 10:34:05 +0100

    changed the options of snmpwalk for --snmphelp

--- plugins-scripts/NWC/Device.pm
+++ plugins-scripts/NWC/Device.pm
@@ -118,7 +118,7 @@ sub init {
       $name = sprintf "/tmp/snmpwalk_%s_%s", $name, $self->opts->hostname;
       printf "rm -f %s\n", $name;
       foreach ($self->trees) {
-        printf "snmpwalk -On -v%s -c %s %s %s >> %s\n", 
+        printf "snmpwalk -ObentU -v%s -c %s %s %s >> %s\n", 
             $self->opts->protocol,
             $self->opts->community,
             $self->opts->hostname,
