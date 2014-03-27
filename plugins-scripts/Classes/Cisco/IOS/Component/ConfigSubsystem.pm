da6675bfa8e98886000db62058ac8084a6883537
Maarten Hoogveld <maarten@hoogveld.org>
2014-03-27 22:40:33 +0100

    Removed the use of ccmHistoryRunningLastSaved checking for cisco configs. This value updated to the current time e.g. on the command 'sh run' and thus there's no value in checking this OID (as far as I can see.) Now only produces a warning/critical if the current running config is ahead of the startup config for more than X minutes. Also fixed the mitigation parameter. Using this parameter didn't have any effect on the outcome of the check.

--- plugins-scripts/Classes/Cisco/IOS/Component/ConfigSubsystem.pm
+++ plugins-scripts/Classes/Cisco/IOS/Component/ConfigSubsystem.pm
@@ -1,6 +1,7 @@
 package Classes::Cisco::IOS::Component::ConfigSubsystem;
 our @ISA = qw(GLPlugin::Item);
 use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
 
 sub init {
   my $self = shift;
@@ -28,27 +29,10 @@ sub check {
   # ccmHistoryStartupLastChanged 
   $self->set_thresholds(warning => 3600, critical => 3600*24);
 
-  # ??? 
-  # The Running config is stored in RAM, so it's lost either way if changed,
-  # unless copied to the startup config (which is saved in NVRAM.)
-  # Don't understand the use of unsaved_since (using Cisco's like 2901, 3750)
-  # ???
-
-  # how much is ccmHistoryRunningLastChanged ahead of ccmHistoryRunningLastSaved
-  # the current running config is definitively lost in case of an outage
-  my $unsaved_since =
-      $self->{ccmHistoryRunningLastChanged} > $self->{ccmHistoryRunningLastSaved} ?
-      time - $self->{ccmHistoryRunningLastChanged} : 0;
-
   # How much is ccmHistoryRunningLastChanged ahead of ccmHistoryStartupLastChanged
   # the running config could have been saved for backup purposes.
   # The saved config can still be identical to the saved running config.
-  # If there are regular backups of the running config and no one messes
-  # with the latter without flushing it to the startup config, then i recommend
-  # to use --mitigation ok. This can be in an environment, where there is
-  # a specific day of the week reserved for maintenance and admins are forced
-  # to save their modifications to the startup-config.
-  my $unsynced_since = 
+  my $unsaved_since = 
       $self->{ccmHistoryRunningLastChanged} > $self->{ccmHistoryStartupLastChanged} ?
       time - $self->{ccmHistoryRunningLastChanged} : 0;
   if ($unsaved_since) {
@@ -57,15 +41,13 @@ sub check {
   } else {
     $self->add_info("saved config is up to date");
   }
-  $self->add_message($self->check_thresholds($unsaved_since));
-  if ($unsynced_since) {
-    my $errorlevel = $self->check_thresholds($unsynced_since);
+  if ($unsaved_since) {
+    my $errorlevel = $self->check_thresholds($unsaved_since);
     if ($errorlevel != OK && defined $self->opts->mitigation()) {
       $errorlevel = $self->opts->mitigation();
     }
-
     $self->add_info(sprintf "saved running config is ahead of startup config since %d minutes. device will boot with a config different from the one which was last saved",
-        $unsynced_since / 60);
+        $unsaved_since / 60);
     $self->add_message($errorlevel);
   }
 }
@@ -78,5 +60,4 @@ sub dump {
     printf "%s: %s %s\n", $_, $self->{$_}, scalar localtime $self->{$_};
   }
 }
-
 
