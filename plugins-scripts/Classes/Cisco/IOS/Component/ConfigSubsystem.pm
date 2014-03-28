91d9d6521f52fb3f67e9d82916ed7b2a998c158f
Maarten Hoogveld <m.hoogveld@elevate.nl>
2014-03-28 16:13:11 +0100

    Clarified the code a bit and fixed bug in last commit

--- plugins-scripts/Classes/Cisco/IOS/Component/ConfigSubsystem.pm
+++ plugins-scripts/Classes/Cisco/IOS/Component/ConfigSubsystem.pm
@@ -32,23 +32,29 @@ sub check {
 
   # How much is ccmHistoryRunningLastChanged ahead of ccmHistoryStartupLastChanged
   # the running config could have been saved for backup purposes.
-  # The saved config can still be identical to the saved running config.
+  # The saved config can still be identical to the running config.
+
+  my $runningChangedDuration = time - $self->{ccmHistoryRunningLastChanged};
+  my $startupChangedDuration = time - $self->{ccmHistoryStartupLastChanged};
 
   # If running config has changed after the startup config
-  if ($self->{ccmHistoryRunningLastChanged} > $self->{ccmHistoryStartupLastChanged}) {
+  if ($runningChangedDuration < $startupChangedDuration) {
     # If running config has changed since boot (with 60 second margin)
     # The margin is use because after a reload the running config is ahead of the startup config by 20-40 seconds
-    if ((time - $self->{ccmHistoryRunningLastChanged}) > ($self->uptime() + $runChangedMarginAfterReload)) {
-      my $unsaved_since = time - $self->{ccmHistoryRunningLastChanged};
+    if (($runningChangedDuration + $runChangedMarginAfterReload) < $self->uptime()) {
+      my $unsaved_since = $runningChangedDuration;
       my $errorlevel = $self->check_thresholds($unsaved_since);
+
       if ($errorlevel != OK && defined $self->opts->mitigation()) {
         $errorlevel = $self->opts->mitigation();
       }
+
       $self->add_info(sprintf "saved running config is ahead of startup config since %d minutes. device will boot with a config different from the one which was last saved",
           $unsaved_since / 60);
+
       $self->add_message($errorlevel);
     } else {
-      $self->add_info(sprintf("running config has not changed since (%d seconds after) reload", $runChangedMarginAfterReload));
+      $self->add_info(sprintf("running config has not changed since reload (using a %d second margin)", $runChangedMarginAfterReload));
     }
   } else {
     $self->add_info("saved config is up to date");
