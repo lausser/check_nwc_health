936ef85b2c0312110b2d2c78a68d447a6ede570b
Gerhard Lausser <gerhard.lausser@consol.de>
2014-11-09 02:41:14 +0100

    paloalto sensors (entity-sensor-mib)

--- plugins-scripts/Classes/PaloAlto.pm
+++ plugins-scripts/Classes/PaloAlto.pm
@@ -5,10 +5,10 @@ use strict;
 sub init {
   my $self = shift;
   if ($self->mode =~ /device::hardware::health/) {
+    $self->analyze_and_check_environmental_subsystem("Classes::ENTITYSENSORMIB::Component::EnvironmentalSubsystem");
+  } elsif ($self->mode =~ /device::hardware::load/) {
     # CPU util on management plane
     # Utilization of CPUs on dataplane that are used for system functions
-    $self->analyze_and_check_environmental_subsystem("Classes::HOSTRESOURCESMIB::Component::EnvironmentalSubsystem");
-  } elsif ($self->mode =~ /device::hardware::load/) {
     $self->analyze_and_check_cpu_subsystem("Classes::HOSTRESOURCESMIB::Component::CpuSubsystem");
   } elsif ($self->mode =~ /device::hardware::memory/) {
     $self->analyze_and_check_mem_subsystem("Classes::HOSTRESOURCESMIB::Component::MemSubsystem");
