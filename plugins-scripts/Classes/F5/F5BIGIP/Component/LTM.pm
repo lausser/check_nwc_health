31cabf649abb2a9b84ca996b867424054cd24619
Gerhard Lausser <gerhard.lausser@consol.de>
2015-02-26 18:42:20 +0100

    add f5 pool-connections and some enhancements for pool-completeness

--- plugins-scripts/Classes/F5/F5BIGIP/Component/LTM.pm
+++ plugins-scripts/Classes/F5/F5BIGIP/Component/LTM.pm
@@ -44,6 +44,11 @@ package Classes::F5::F5BIGIP::Component::LTMSubsystem9;
 our @ISA = qw(Classes::F5::F5BIGIP::Component::LTMSubsystem GLPlugin::SNMP::TableItem);
 use strict;
 
+#
+# A node is an ip address (may belong to more than one pool)
+# A pool member is an ip:port combination
+#
+
 sub init {
   my $self = shift;
   # ! merge ltmPoolStatus, ltmPoolMemberStatus, bec. ltmPoolAvailabilityState is deprecated
@@ -59,67 +64,111 @@ sub init {
       'F5-BIGIP-LOCAL-MIB', 'ltmPoolStatusTable', 'ltmPoolStatusName')) {
     push(@auxpools, $_);
   }
+  if (! grep { $self->filter_name($_->{ltmPoolStatusName}) } @auxpools) {
+    #$self->add_unknown("did not find any pools");
+    $self->{pools} = [];
+    return;
+  }
   my @auxstats = ();
   foreach ($self->get_snmp_table_objects_with_cache(
       'F5-BIGIP-LOCAL-MIB', 'ltmPoolStatTable', 'ltmPoolStatName')) {
-    push(@auxstats, $_);
+    push(@auxstats, $_) if $self->filter_name($_->{ltmPoolStatName});
   }
   foreach ($self->get_snmp_table_objects_with_cache(
       'F5-BIGIP-LOCAL-MIB', 'ltmPoolTable', 'ltmPoolName')) {
-    if ($self->filter_name($_->{ltmPoolName})) {
-      foreach my $auxpool (@auxpools) {
-        if ($_->{ltmPoolName} eq $auxpool->{ltmPoolStatusName}) {
-          foreach my $key (keys %{$auxpool}) {
-            $_->{$key} = $auxpool->{$key};
-          }
+    foreach my $auxpool (@auxpools) {
+      if ($_->{ltmPoolName} eq $auxpool->{ltmPoolStatusName}) {
+        foreach my $key (keys %{$auxpool}) {
+          $_->{$key} = $auxpool->{$key};
         }
       }
-      foreach my $auxstat (@auxstats) {
-        if ($_->{ltmPoolName} eq $auxstat->{ltmPoolStatName}) {
-          foreach my $key (keys %{$auxstat}) {
-            $_->{$key} = $auxstat->{$key};
-          }
-        }
-      }
-      push(@{$self->{pools}},
-          Classes::F5::F5BIGIP::Component::LTMSubsystem9::LTMPool->new(%{$_}));
     }
+    foreach my $auxstat (@auxstats) {
+      if ($_->{ltmPoolName} eq $auxstat->{ltmPoolStatName}) {
+        foreach my $key (keys %{$auxstat}) {
+          $_->{$key} = $auxstat->{$key};
+        }
+      }
+    }
+    push(@{$self->{pools}},
+        Classes::F5::F5BIGIP::Component::LTMSubsystem9::LTMPool->new(%{$_}));
   }
-  my @auxmembers = ();
+  my @auxpoolmbrstatus = ();
   foreach ($self->get_snmp_table_objects_with_cache(
       'F5-BIGIP-LOCAL-MIB', 'ltmPoolMbrStatusTable', 'ltmPoolMbrStatusPoolName')) {
     next if ! defined $_->{ltmPoolMbrStatusPoolName};
     $_->{ltmPoolMbrStatusAddr} = $self->unhex_ip($_->{ltmPoolMbrStatusAddr});
-    push(@auxmembers, $_);
+    push(@auxpoolmbrstatus, $_);
   }
-  my @auxaddrs = ();
-  foreach ($self->get_snmp_table_objects(
-      'F5-BIGIP-LOCAL-MIB', 'ltmNodeAddrStatusTable')) {
-    $_->{ltmNodeAddrStatusAddr} = $self->unhex_ip($_->{ltmNodeAddrStatusAddr});
-    push(@auxaddrs, $_);
+  my @auxpoolmemberstat = ();
+  foreach ($self->get_snmp_table_objects_with_cache(
+      'F5-BIGIP-LOCAL-MIB', 'ltmPoolMemberStatTable', 'ltmPoolMemberStatPoolName')) {
+    $_->{ltmPoolMemberStatAddr} = $self->unhex_ip($_->{ltmPoolMemberStatAddr});
+    push(@auxpoolmemberstat, $_);
+    # ltmPoolMemberStatAddr is deprecated, use ltmPoolMemberStatNodeName
   }
   foreach ($self->get_snmp_table_objects_with_cache(
       'F5-BIGIP-LOCAL-MIB', 'ltmPoolMemberTable', 'ltmPoolMemberPoolName')) {
-    if ($self->filter_name($_->{ltmPoolMemberPoolName})) {
-      $_->{ltmPoolMemberAddr} = $self->unhex_ip($_->{ltmPoolMemberAddr});
-      foreach my $auxmember (@auxmembers) {
-        if ($_->{ltmPoolMemberPoolName} eq $auxmember->{ltmPoolMbrStatusPoolName} &&
-            $_->{ltmPoolMemberAddrType} eq $auxmember->{ltmPoolMbrStatusAddrType} &&
-            $_->{ltmPoolMemberAddr} eq $auxmember->{ltmPoolMbrStatusAddr}) {
-          foreach my $key (keys %{$auxmember}) {
-            $_->{$key} = $auxmember->{$key};
-          }
+    $_->{ltmPoolMemberAddr} = $self->unhex_ip($_->{ltmPoolMemberAddr});
+    foreach my $auxmbr (@auxpoolmbrstatus) {
+      if ($_->{ltmPoolMemberPoolName} eq $auxmbr->{ltmPoolMbrStatusPoolName} &&
+          $_->{ltmPoolMemberPort} eq $auxmbr->{ltmPoolMbrStatusPort} &&
+          $_->{ltmPoolMemberAddrType} eq $auxmbr->{ltmPoolMbrStatusAddrType} &&
+          $_->{ltmPoolMemberAddr} eq $auxmbr->{ltmPoolMbrStatusAddr}) {
+        foreach my $key (keys %{$auxmbr}) {
+          next if $key =~ /.*indices$/;
+          $_->{$key} = $auxmbr->{$key};
         }
       }
-      foreach my $auxaddr (@auxaddrs) {
-        if ($_->{ltmPoolMemberAddrType} eq $auxaddr->{ltmNodeAddrStatusAddrType} &&
-            $_->{ltmPoolMemberAddr} eq $auxaddr->{ltmNodeAddrStatusAddr}) {
-          $_->{ltmNodeAddrStatusName} = $auxaddr->{ltmNodeAddrStatusName};
-        }
-      }
-      push(@{$self->{poolmembers}},
-          Classes::F5::F5BIGIP::Component::LTMSubsystem9::LTMPoolMember->new(%{$_}));
     }
+    foreach my $auxmember (@auxpoolmemberstat) {
+      if ($_->{ltmPoolMemberPoolName} eq $auxmember->{ltmPoolMemberStatPoolName} &&
+          $_->{ltmPoolMemberPort} eq $auxmember->{ltmPoolMemberStatPort} &&
+          $_->{ltmPoolMemberNodeName} eq $auxmember->{ltmPoolMemberStatNodeName}) {
+        foreach my $key (keys %{$auxmember}) {
+          next if $key =~ /.*indices$/;
+          $_->{$key} = $auxmember->{$key};
+        }
+      }
+    }
+    push(@{$self->{poolmembers}},
+        Classes::F5::F5BIGIP::Component::LTMSubsystem9::LTMPoolMember->new(%{$_}));
+  }
+  # ltmPoolMemberNodeName may be the same as ltmPoolMemberAddr
+  # there is a chance to learn the actual hostname via ltmNodeAddrStatusName
+  # so if there ia a member with name==addr, we get the addrstatus table
+  my $need_name_from_addr = 0;
+  foreach my $poolmember (@{$self->{poolmembers}}) {
+    if ($poolmember->{ltmPoolMemberNodeName} eq $poolmember->{ltmPoolMemberAddr}) {
+      $need_name_from_addr = 1;
+    }
+  }
+  if ($need_name_from_addr) {
+    my @auxnodeaddrstatus = ();
+    foreach ($self->get_snmp_table_objects(
+        'F5-BIGIP-LOCAL-MIB', 'ltmNodeAddrStatusTable')) {
+      $_->{ltmNodeAddrStatusAddr} = $self->unhex_ip($_->{ltmNodeAddrStatusAddr});
+      push(@auxnodeaddrstatus, $_);
+    }
+    foreach my $poolmember (@{$self->{poolmembers}}) {
+      foreach my $auxaddr (@auxnodeaddrstatus) {
+        if ($poolmember->{ltmPoolMemberAddrType} eq $auxaddr->{ltmNodeAddrStatusAddrType} &&
+            $poolmember->{ltmPoolMemberAddr} eq $auxaddr->{ltmNodeAddrStatusAddr}) {
+          $poolmember->{ltmNodeAddrStatusName} = $auxaddr->{ltmNodeAddrStatusName};
+          last;
+          # needed later, if ltmNodeAddrStatusName is an ip-address. LTMPoolMember::finish
+        }
+      }
+    }
+  } else {
+    foreach my $poolmember (@{$self->{poolmembers}}) {
+      # because later we use ltmNodeAddrStatusName
+      $poolmember->{ltmNodeAddrStatusName} = $poolmember->{ltmPoolMemberNodeName};
+      my $x = 1;
+    }
+  }
+  foreach my $poolmember (@{$self->{poolmembers}}) {
+    $poolmember->rename();
   }
   $self->assign_members_to_pools();
 }
@@ -157,81 +206,87 @@ sub finish {
 
 sub check {
   my $self = shift;
-  $self->add_info(sprintf "pool %s is %s, avail state is %s, active members: %d of %d", 
-      $self->{ltmPoolName},
-      $self->{ltmPoolStatusEnabledState}, $self->{ltmPoolStatusAvailState},
-      $self->{ltmPoolActiveMemberCnt}, $self->{ltmPoolMemberCnt});
-  if ($self->{ltmPoolActiveMemberCnt} == 1) {
-    # only one member left = no more redundancy!!
-    $self->set_thresholds(warning => "100:", critical => "51:");
-  } else {
-    $self->set_thresholds(warning => "51:", critical => "26:");
-  }
-  my $message = sprintf ("pool %s has %d active members (of %d) and %d sessions",
-          $self->{ltmPoolName},
-          $self->{ltmPoolActiveMemberCnt}, $self->{ltmPoolMemberCnt},
-          $self->{ltmPoolStatServerCurConns});
-  $self->add_message($self->check_thresholds($self->{completeness}), $message);
-  if ($self->{ltmPoolMinActiveMembers} > 0 &&
-      $self->{ltmPoolActiveMemberCnt} < $self->{ltmPoolMinActiveMembers}) {
-    $message = sprintf("pool %s has not enough active members (%d, min is %d)",
-            $self->{ltmPoolName}, $self->{ltmPoolActiveMemberCnt},
-            $self->{ltmPoolMinActiveMembers});
-    $self->add_message(defined $self->opts->mitigation() ? $self->opts->mitigation() : CRITICAL, $message);
-  }
-  if ($self->check_messages()) {
+  if ($self->mode =~ /device::lb::pool::completeness/) {
+    $self->add_info(sprintf "pool %s is %s, avail state is %s, active members: %d of %d", 
+        $self->{ltmPoolName},
+        $self->{ltmPoolStatusEnabledState}, $self->{ltmPoolStatusAvailState},
+        $self->{ltmPoolActiveMemberCnt}, $self->{ltmPoolMemberCnt});
+    if ($self->{ltmPoolActiveMemberCnt} == 1) {
+      # only one member left = no more redundancy!!
+      $self->set_thresholds(warning => "100:", critical => "51:");
+    } else {
+      $self->set_thresholds(warning => "51:", critical => "26:");
+    }
+    my $message = sprintf ("pool %s has %d active members (of %d) and %d sessions",
+            $self->{ltmPoolName},
+            $self->{ltmPoolActiveMemberCnt}, $self->{ltmPoolMemberCnt},
+            $self->{ltmPoolStatServerCurConns});
+    $self->add_message($self->check_thresholds($self->{completeness}), $message);
+    if ($self->{ltmPoolMinActiveMembers} > 0 &&
+        $self->{ltmPoolActiveMemberCnt} < $self->{ltmPoolMinActiveMembers}) {
+      $message = sprintf("pool %s has not enough active members (%d, min is %d)",
+              $self->{ltmPoolName}, $self->{ltmPoolActiveMemberCnt},
+              $self->{ltmPoolMinActiveMembers});
+      $self->add_message(defined $self->opts->mitigation() ? $self->opts->mitigation() : CRITICAL, $message);
+    }
+    if ($self->check_messages()) {
+      foreach my $member (@{$self->{members}}) {
+        $member->check();
+      }
+    }
+    $self->add_perfdata(
+        label => sprintf('pool_%s_completeness', $self->{ltmPoolName}),
+        value => $self->{completeness},
+        uom => '%',
+    );
+    $self->add_perfdata(
+        label => sprintf('pool_%s_servercurconns', $self->{ltmPoolName}),
+        value => $self->{ltmPoolStatServerCurConns},
+        warning => undef, critical => undef,
+    );
+    if ($self->opts->report eq "html") {
+      printf "%s - %s%s\n", $self->status_code($self->check_messages()), $message, $self->perfdata_string() ? " | ".$self->perfdata_string() : "";
+      $self->suppress_messages();
+      printf "<table style=\"border-collapse:collapse; border: 1px solid black;\">";
+      printf "<tr>";
+      foreach (qw(Node Port Enabled Avail Reason)) {
+        printf "<th style=\"text-align: left; padding-left: 4px; padding-right: 6px;\">%s</th>", $_;
+      }
+      printf "</tr>";
+      foreach (sort {$a->{ltmPoolMemberNodeName} cmp $b->{ltmPoolMemberNodeName}} @{$self->{members}}) {
+        printf "<tr>";
+        printf "<tr style=\"border: 1px solid black;\">";
+        foreach my $attr (qw(ltmPoolMemberNodeName ltmPoolMemberPort ltmPoolMbrStatusEnabledState ltmPoolMbrStatusAvailState ltmPoolMbrStatusDetailReason)) {
+          if ($_->{ltmPoolMbrStatusEnabledState} eq "enabled") {
+            if ($_->{ltmPoolMbrStatusAvailState} eq "green") {
+              printf "<td style=\"text-align: left; padding-left: 4px; padding-right: 6px; background-color: #33ff00;\">%s</td>", $_->{$attr};
+            } else {
+              printf "<td style=\"text-align: left; padding-left: 4px; padding-right: 6px; background-color: #f83838;\">%s</td>", $_->{$attr};
+            }
+          } else {
+            printf "<td style=\"text-align: left; padding-left: 4px; padding-right: 6px; background-color: #acacac;\">%s</td>", $_->{$attr};
+          }
+        }
+        printf "</tr>";
+      }
+      printf "</table>\n";
+      printf "<!--\nASCII_NOTIFICATION_START\n";
+      foreach (qw(Node Port Enabled Avail Reason)) {
+        printf "%20s", $_;
+      }
+      printf "\n";
+      foreach (sort {$a->{ltmPoolMemberNodeName} cmp $b->{ltmPoolMemberNodeName}} @{$self->{members}}) {
+        foreach my $attr (qw(ltmPoolMemberNodeName ltmPoolMemberPort ltmPoolMbrStatusEnabledState ltmPoolMbrStatusAvailState ltmPoolMbrStatusDetailReason)) {
+          printf "%20s", $_->{$attr};
+        }
+        printf "\n";
+      }
+      printf "ASCII_NOTIFICATION_END\n-->\n";
+    }
+  } elsif ($self->mode =~ /device::lb::pool::connections/) {
     foreach my $member (@{$self->{members}}) {
       $member->check();
     }
-  }
-  $self->add_perfdata(
-      label => sprintf('pool_%s_completeness', $self->{ltmPoolName}),
-      value => $self->{completeness},
-      uom => '%',
-  );
-  $self->add_perfdata(
-      label => sprintf('pool_%s_servercurconns', $self->{ltmPoolName}),
-      value => $self->{ltmPoolStatServerCurConns},
-      warning => undef, critical => undef,
-  );
-  if ($self->opts->report eq "html") {
-    printf "%s - %s%s\n", $self->status_code($self->check_messages()), $message, $self->perfdata_string() ? " | ".$self->perfdata_string() : "";
-    $self->suppress_messages();
-    printf "<table style=\"border-collapse:collapse; border: 1px solid black;\">";
-    printf "<tr>";
-    foreach (qw(Name Enabled Avail Reason)) {
-      printf "<th style=\"text-align: left; padding-left: 4px; padding-right: 6px;\">%s</th>", $_;
-    }
-    printf "</tr>";
-    foreach (sort {$a->{ltmPoolMemberNodeName} cmp $b->{ltmPoolMemberNodeName}} @{$self->{members}}) {
-      printf "<tr>";
-      printf "<tr style=\"border: 1px solid black;\">";
-      foreach my $attr (qw(ltmPoolMemberNodeName ltmPoolMbrStatusEnabledState ltmPoolMbrStatusAvailState ltmPoolMbrStatusDetailReason)) {
-        if ($_->{ltmPoolMbrStatusEnabledState} eq "enabled") {
-          if ($_->{ltmPoolMbrStatusAvailState} eq "green") {
-            printf "<td style=\"text-align: left; padding-left: 4px; padding-right: 6px; background-color: #33ff00;\">%s</td>", $_->{$attr};
-          } else {
-            printf "<td style=\"text-align: left; padding-left: 4px; padding-right: 6px; background-color: #f83838;\">%s</td>", $_->{$attr};
-          }
-        } else {
-          printf "<td style=\"text-align: left; padding-left: 4px; padding-right: 6px; background-color: #acacac;\">%s</td>", $_->{$attr};
-        }
-      }
-      printf "</tr>";
-    }
-    printf "</table>\n";
-    printf "<!--\nASCII_NOTIFICATION_START\n";
-    foreach (qw(Name Enabled Avail Reason)) {
-      printf "%20s", $_;
-    }
-    printf "\n";
-    foreach (sort {$a->{ltmPoolMemberNodeName} cmp $b->{ltmPoolMemberNodeName}} @{$self->{members}}) {
-      foreach my $attr (qw(ltmPoolMemberNodeName ltmPoolMbrStatusEnabledState ltmPoolMbrStatusAvailState ltmPoolMbrStatusDetailReason)) {
-        printf "%20s", $_->{$attr};
-      }
-      printf "\n";
-    }
-    printf "ASCII_NOTIFICATION_END\n-->\n";
   }
 }
 
@@ -243,25 +298,58 @@ use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
 
 sub finish {
   my $self = shift;
-  $self->{ltmPoolMemberNodeName} ||= $self->{ltmPoolMemberAddr};
-  if ($self->{ltmPoolMemberNodeName} eq $self->{ltmPoolMemberAddr} &&
-      $self->{ltmNodeAddrStatusName}) {
-    $self->{ltmPoolMemberNodeName} = $self->{ltmNodeAddrStatusName};
+  if ($self->mode =~ /device::lb::pool::completeness/) {
+    $self->{ltmPoolMemberNodeName} ||= $self->{ltmPoolMemberAddr};
+  } elsif ($self->mode =~ /device::lb::pool::connections/) {
+    $self->{ltmPoolMemberStatServerPctConns} = ($self->{ltmPoolMemberStatServerMaxConns} == 0) ? 0 :
+        100 * $self->{ltmPoolMemberStatServerCurConns} / $self->{ltmPoolMemberStatServerMaxConns};
+  }
+}
+
+sub rename {
+  my $self = shift;
+  if ($self->mode =~ /device::lb::pool::completeness/) {
+    if ($self->{ltmPoolMemberNodeName} eq $self->{ltmPoolMemberAddr} &&
+        $self->{ltmNodeAddrStatusName}) {
+      $self->{ltmPoolMemberNodeName} = $self->{ltmNodeAddrStatusName};
+    }
   }
 }
 
 sub check {
   my $self = shift;
-  if ($self->{ltmPoolMbrStatusEnabledState} eq "enabled") {
-    if ($self->{ltmPoolMbrStatusAvailState} ne "green") {
-      # info only, because it would ruin thresholds in the pool
-      $self->add_ok(sprintf 
-          "member %s is %s/%s (%s)",
-          $self->{ltmPoolMemberNodeName},
-          $self->{ltmPoolMemberMonitorState},
-          $self->{ltmPoolMbrStatusAvailState},
-          $self->{ltmPoolMbrStatusDetailReason});
+  if ($self->mode =~ /device::lb::pool::completeness/) {
+    if ($self->{ltmPoolMbrStatusEnabledState} eq "enabled") {
+      if ($self->{ltmPoolMbrStatusAvailState} ne "green") {
+        # info only, because it would ruin thresholds in the pool
+        $self->add_ok(sprintf 
+            "member %s:%s is %s/%s (%s)",
+            $self->{ltmPoolMemberNodeName},
+            $self->{ltmPoolMemberPort},
+            $self->{ltmPoolMemberMonitorState},
+            $self->{ltmPoolMbrStatusAvailState},
+            $self->{ltmPoolMbrStatusDetailReason});
+      }
     }
+  } elsif ($self->mode =~ /device::lb::pool::connections/) {
+    my $label = $self->{ltmPoolMemberNodeName}.'_'.$self->{ltmPoolMemberPort};
+    $self->set_thresholds(metric => $label.'_connections_pct', warning => "85", critical => "95");
+    $self->add_info(sprintf "member %s:%s has %d connections (from max %d)",
+        $self->{ltmPoolMemberNodeName},
+        $self->{ltmPoolMemberPort},
+        $self->{ltmPoolMemberStatServerCurConns},
+        $self->{ltmPoolMemberStatServerMaxConns});
+    $self->add_message($self->check_thresholds(metric => $label.'_connections_pct', value => $self->{ltmPoolMemberStatServerPctConns}));
+    $self->add_perfdata(
+        label => $label.'_connections_pct',
+        value => $self->{ltmPoolMemberStatServerPctConns},
+        uom => '%',
+    );
+    $self->add_perfdata(
+        label => $label.'_connections',
+        value => $self->{ltmPoolMemberStatServerCurConns},
+        warning => undef, critical => undef,
+    );
   }
 }
 
