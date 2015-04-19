afda9417c60b5724757efe8df55517daaadcfcf7
Gerhard Lausser <gerhard.lausser@consol.de>
2015-04-19 23:00:58 +0200

    better performance data for linuxlocal interfaces

--- plugins-scripts/Classes/Server/Linux.pm
+++ plugins-scripts/Classes/Server/Linux.pm
@@ -109,14 +109,38 @@ sub finish {
       # vlan graffl
       $self->{inputUtilization} = 0;
       $self->{outputUtilization} = 0;
+      $self->{maxInputRate} = 0;
+      $self->{maxOutputRate} = 0;
     } else {
       $self->{inputUtilization} = $self->{delta_ifInOctets} * 8 * 100 /
           ($self->{delta_timestamp} * $self->{ifSpeed});
       $self->{outputUtilization} = $self->{delta_ifOutOctets} * 8 * 100 /
           ($self->{delta_timestamp} * $self->{ifSpeed});
+      $self->{maxInputRate} = $self->{ifSpeed};
+      $self->{maxOutputRate} = $self->{ifSpeed};
+    }
+    if (defined $self->opts->ifspeedin) {
+      $self->{inputUtilization} = $self->{delta_ifInOctets} * 8 * 100 /
+          ($self->{delta_timestamp} * $self->opts->ifspeedin);
+      $self->{maxInputRate} = $self->opts->ifspeedin;
+    }
+    if (defined $self->opts->ifspeedout) {
+      $self->{outputUtilization} = $self->{delta_ifOutOctets} * 8 * 100 /
+          ($self->{delta_timestamp} * $self->opts->ifspeedout);
+      $self->{maxOutputRate} = $self->opts->ifspeedout;
+    }
+    if (defined $self->opts->ifspeed) {
+      $self->{inputUtilization} = $self->{delta_ifInOctets} * 8 * 100 /
+          ($self->{delta_timestamp} * $self->opts->ifspeed);
+      $self->{outputUtilization} = $self->{delta_ifOutOctets} * 8 * 100 /
+          ($self->{delta_timestamp} * $self->opts->ifspeed);
+      $self->{maxInputRate} = $self->opts->ifspeed;
+      $self->{maxOutputRate} = $self->opts->ifspeed;
     }
     $self->{inputRate} = $self->{delta_ifInOctets} / $self->{delta_timestamp};
     $self->{outputRate} = $self->{delta_ifOutOctets} / $self->{delta_timestamp};
+    $self->{maxInputRate} /= 8; # auf octets umrechnen wie die in/out
+    $self->{maxOutputRate} /= 8;
     my $factor = 1/8; # default Bits
     if ($self->opts->units) {
       if ($self->opts->units eq "GB") {
@@ -139,19 +163,19 @@ sub finish {
     }
     $self->{inputRate} /= $factor;
     $self->{outputRate} /= $factor;
+    $self->{maxInputRate} /= $factor;
+    $self->{maxOutputRate} /= $factor;
   } elsif ($self->mode =~ /device::interfaces::errors/) {
-    $self->valdiff({name => $self->{ifDescr}}, qw(ifInErrors ifOutErrors ifInDiscards ifOutDiscards));
-    $self->{inputErrorRate} = $self->{delta_ifInErrors} 
+    $self->valdiff({name => $self->{ifDescr}}, qw(ifInErrors ifOutErrors));
+    $self->{inputErrorRate} = $self->{delta_ifInErrors}
         / $self->{delta_timestamp};
-    $self->{outputErrorRate} = $self->{delta_ifOutErrors} 
+    $self->{outputErrorRate} = $self->{delta_ifOutErrors}
         / $self->{delta_timestamp};
-    $self->{inputDiscardRate} = $self->{delta_ifInDiscards} 
+  } elsif ($self->mode =~ /device::interfaces::discards/) {
+    $self->valdiff({name => $self->{ifDescr}}, qw(ifInDiscards ifOutDiscards));
+    $self->{inputDiscardRate} = $self->{delta_ifInDiscards}
         / $self->{delta_timestamp};
-    $self->{outputDiscardRate} = $self->{delta_ifOutDiscards} 
-        / $self->{delta_timestamp};
-    $self->{inputRate} = ($self->{delta_ifInErrors} + $self->{delta_ifInDiscards}) 
-        / $self->{delta_timestamp};
-    $self->{outputRate} = ($self->{delta_ifOutErrors} + $self->{delta_ifOutDiscards}) 
+    $self->{outputDiscardRate} = $self->{delta_ifOutDiscards}
         / $self->{delta_timestamp};
   } elsif ($self->mode =~ /device::interfaces::operstatus/) {
   }
@@ -163,41 +187,69 @@ sub check {
   if ($self->mode =~ /device::interfaces::traffic/) {
   } elsif ($self->mode =~ /device::interfaces::usage/) {
     $self->add_info(sprintf 'interface %s usage is in:%.2f%% (%s) out:%.2f%% (%s)',
-        $self->{ifDescr}, 
-        $self->{inputUtilization}, 
+        $self->{ifDescr},
+        $self->{inputUtilization},
         sprintf("%.2f%s/s", $self->{inputRate},
             ($self->opts->units ? $self->opts->units : 'Bits')),
         $self->{outputUtilization},
         sprintf("%.2f%s/s", $self->{outputRate},
             ($self->opts->units ? $self->opts->units : 'Bits')));
-    $self->set_thresholds(warning => 80, critical => 90);
-    my $in = $self->check_thresholds($self->{inputUtilization});
-    my $out = $self->check_thresholds($self->{outputUtilization});
+    $self->set_thresholds(
+        metric => $self->{ifDescr}.'_usage_in',
+        warning => 80,
+        critical => 90
+    );
+    my $in = $self->check_thresholds(
+        metric => $self->{ifDescr}.'_usage_in',
+        value => $self->{inputUtilization}
+    );
+    $self->set_thresholds(
+        metric => $self->{ifDescr}.'_usage_out',
+        warning => 80,
+        critical => 90
+    );
+    my $out = $self->check_thresholds(
+        metric => $self->{ifDescr}.'_usage_out',
+        value => $self->{outputUtilization}
+    );
     my $level = ($in > $out) ? $in : ($out > $in) ? $out : $in;
     $self->add_message($level);
     $self->add_perfdata(
         label => $self->{ifDescr}.'_usage_in',
         value => $self->{inputUtilization},
         uom => '%',
-        warning => $self->{warning},
-        critical => $self->{critical},
     );
     $self->add_perfdata(
         label => $self->{ifDescr}.'_usage_out',
         value => $self->{outputUtilization},
         uom => '%',
-        warning => $self->{warning},
-        critical => $self->{critical},
+    );
+
+    my ($inwarning, $incritical) = $self->get_thresholds(
+        metric => $self->{ifDescr}.'_usage_in',
     );
     $self->add_perfdata(
         label => $self->{ifDescr}.'_traffic_in',
         value => $self->{inputRate},
         uom => $self->opts->units,
+        places => 2,
+        min => 0,
+        max => $self->{maxInputRate},
+        warning => $self->{maxInputRate} / 100 * $inwarning,
+        critical => $self->{maxInputRate} / 100 * $incritical,
+    );
+    my ($outwarning, $outcritical) = $self->get_thresholds(
+        metric => $self->{ifDescr}.'_usage_out',
     );
     $self->add_perfdata(
         label => $self->{ifDescr}.'_traffic_out',
         value => $self->{outputRate},
         uom => $self->opts->units,
+        places => 2,
+        min => 0,
+        max => $self->{maxOutputRate},
+        warning => $self->{maxOutputRate} / 100 * $outwarning,
+        critical => $self->{maxOutputRate} / 100 * $outcritical,
     );
   } elsif ($self->mode =~ /device::interfaces::errors/) {
     $self->add_info(sprintf 'interface %s errors in:%.2f/s out:%.2f/s ',
