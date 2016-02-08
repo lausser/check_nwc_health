501a3cf16985644f58888d3bf4ad40328217d918
Gerhard Lausser <gerhard.lausser@consol.de>
2016-02-08 11:15:37 +0100

    add accidentially removed lmsensors again

---
+++ plugins-scripts/Classes/LMSENSORSMIB/Component/TemperatureSubsystem.pm
@@ -0,0 +1,33 @@
+package Classes::LMSENSORSMIB::Component::TemperatureSubsystem;
+our @ISA = qw(GLPlugin::SNMP::Item);
+use strict;
+
+sub init {
+  my $self = shift;
+  $self->get_snmp_tables('LM-SENSORS-MIB', [
+      ['temperatures', 'lmTempSensorsTable', 'Classes::LMSENSORSMIB::Component::TemperatureSubsystem::Temperature'],
+  ]);
+}
+
+package Classes::LMSENSORSMIB::Component::TemperatureSubsystem::Temperature;
+our @ISA = qw(GLPlugin::SNMP::TableItem);
+use strict;
+
+sub finish {
+  my $self = shift;
+  $self->{lmTempSensorsValue} /= 1000;
+}
+
+sub check {
+  my $self = shift;
+  $self->{ciscoEnvMonTemperatureStatusIndex} ||= 0;
+  $self->add_info(sprintf 'temp %s is %.2fC',
+      $self->{lmTempSensorsDevice},
+      $self->{lmTempSensorsValue});
+  $self->add_ok();
+  $self->add_perfdata(
+      label => sprintf('temp_%s', $self->{lmTempSensorsDevice}),
+      value => $self->{lmTempSensorsValue},
+  );
+}
+
