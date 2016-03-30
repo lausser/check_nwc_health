d824762d4c072adcb4953842b8f0ae66385c7843
Gerhard Lausser <gerhard.lausser@consol.de>
2016-03-30 14:42:53 +0200

    fix lsmpi_io in the new cisco enhanced memory

--- plugins-scripts/Classes/Cisco/CISCOENHANCEDMEMPOOLMIB/Component/MemSubsystem.pm
+++ plugins-scripts/Classes/Cisco/CISCOENHANCEDMEMPOOLMIB/Component/MemSubsystem.pm
@@ -30,7 +30,7 @@ sub check {
   my $self = shift;
   $self->add_info(sprintf 'mempool %s usage is %.2f%%',
       $self->{name}, $self->{usage});
-  if ($self->{name} eq 'lsmpi_io' &&
+  if ($self->{name} =~ /^lsmpi_io/ &&
       $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /IOS.*XE/i) {
     # https://supportforums.cisco.com/docs/DOC-16425
     $self->force_thresholds(
@@ -38,7 +38,7 @@ sub check {
         warning => 100,
         critical => 100,
     );
-  } elsif ($self->{name} eq 'reserved' &&
+  } elsif ($self->{name} =~ /^reserved/ &&
       $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /IOS.*XR/i) {
     # ASR9K "reserved" and "image" are always at 100%
     $self->force_thresholds(
@@ -46,7 +46,7 @@ sub check {
         warning => 100,
         critical => 100,
     );
-  } elsif ($self->{name} eq 'image' &&
+  } elsif ($self->{name} =~ /^image/ &&
       $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /IOS.*XR/i) {
     $self->force_thresholds(
         metric => $self->{name}.'_usage',
