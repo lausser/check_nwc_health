f6144f9ca8e891dd808fa03ad2f1fbd44e0ac3c3
Daniel Hoffend <dh@dotlan.net>
2016-04-05 11:12:12 +0200

    UCDMIB: Updated CpuSubsysten, added LoadSubsystem
    *   The CPU Subsystem now also includes  RawWait, RawKernel, RawInterrupt
        to cpu_usage calculation.
    
    *   Removed unsused unix_init, unix_check and unix_dump subroutines,
        they're not used anywhere the whole project
    
    *   Added LoadSubsystem that offers Load based monitoring. The Load
        Subsystem also checks the ucd error flag/message configured in
        net-snmp (if configured) and will autoconfigure the thresholds
        based on remote configuration

---
+++ plugins-scripts/Classes/UCDMIB/Components/LoadSubsystem.pm
@@ -0,0 +1,62 @@
+package Classes::UCDMIB::Component::LoadSubsystem;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::Item);
+use strict;
+
+sub init {
+  my $self = shift;
+  my %params = @_;
+  my $type = 0;
+  $self->get_snmp_tables('UCD-SNMP-MIB', [
+      ['loads', 'laTable', 'Classes::UCDMIB::Component::LoadSubsystem::Load'],
+  ]);
+}
+
+sub check {
+  my $self = shift;
+  $self->add_info('checking loads');
+  foreach (@{$self->{loads}}) {
+    $_->check();
+  }
+}
+
+sub dump {
+  my $self = shift;
+  foreach (@{$self->{loads}}) {
+    $_->dump();
+  }
+}
+
+
+package Classes::UCDMIB::Component::LoadSubsystem::Load;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
+use strict;
+use Data::Dumper;
+
+sub check {
+  my $self = shift;
+  my $errorfound = 0;
+  $self->set_thresholds(
+      metric => lc $self->{laNames},
+      warning => $self->{laConfig},
+      critical => $self->{laConfig}
+  );
+  $self->add_info(
+      sprintf '%s is %.2f%s',
+      lc $self->{laNames}, $self->{laLoad},
+      $self->{'laErrorFlag'} eq 'error'
+          ? sprintf ' (%s)', $self->{'laErrMessage'}
+          : ''
+  );
+  if ($self->{'laErrorFlag'} eq 'error') {
+    $self->add_message(Monitoring::GLPlugin::CRITICAL);
+  } else {
+    $self->add_message($self->check_thresholds(
+        metric => lc $self->{laNames},
+        value => $self->{laLoad}));
+  }
+  $self->add_perfdata(
+      label => lc $self->{laNames},
+      value => $self->{laLoad},
+  );
+}
+
