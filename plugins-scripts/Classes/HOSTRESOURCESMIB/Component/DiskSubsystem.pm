09975467c2c2d777f6346256407dcab6e37fb2ab
Gerhard Lausser <gerhard.lausser@consol.de>
2016-09-19 13:06:07 +0200

    update ucd-diskio, cumulus

--- plugins-scripts/Classes/HOSTRESOURCESMIB/Component/DiskSubsystem.pm
+++ plugins-scripts/Classes/HOSTRESOURCESMIB/Component/DiskSubsystem.pm
@@ -15,13 +15,16 @@ use strict;
 
 sub check {
   my $self = shift;
-  my $free = 100 - 100 * $self->{hrStorageUsed} / $self->{hrStorageSize};
+  my $free = 100;
+  eval {
+     $free = 100 - 100 * $self->{hrStorageUsed} / $self->{hrStorageSize};
+  };
   $self->add_info(sprintf 'storage %s (%s) has %.2f%% free space left',
       $self->{hrStorageIndex},
       $self->{hrStorageDescr},
       $free);
-  if ($self->{hrStorageDescr} eq "/dev" || $self->{hrStorageDescr} =~ /.*cdrom.*/) {
-    # /dev is usually full, so we ignore it.
+  if ($self->{hrStorageDescr} eq "/dev" || $self->{hrStorageDescr} =~ /.*cdrom.*/ || $self->{hrStorageSize} == 0) {
+    # /dev is usually full, so we ignore it. size 0 is virtual crap
     $self->set_thresholds(metric => sprintf('%s_free_pct', $self->{hrStorageDescr}),
         warning => '0:', critical => '0:');
   } else {
