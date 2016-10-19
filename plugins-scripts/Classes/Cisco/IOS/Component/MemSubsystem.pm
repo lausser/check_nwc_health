5a86983cf0df6e1bf1f36f76c646915053e7a517
Gerhard Lausser <gerhard.lausser@consol.de>
2016-10-19 15:03:45 +0200

    fix memory-usage for Catalyst L3 (which have empty CISCO-ENHANCED-MEMPOOL-MIB)

--- plugins-scripts/Classes/Cisco/IOS/Component/MemSubsystem.pm
+++ plugins-scripts/Classes/Cisco/IOS/Component/MemSubsystem.pm
@@ -6,6 +6,14 @@ sub init {
   my $self = shift;
   if ($self->implements_mib('CISCO-ENHANCED-MEMPOOL-MIB')) {
     $self->analyze_and_check_mem_subsystem("Classes::Cisco::CISCOENHANCEDMEMPOOLMIB::Component::MemSubsystem");
+    if (! exists $self->{mems} || scalar(@{$self->{mems}}) == 0) {
+      # satz mix x....
+      # der hier: Cisco IOS Software, IOS-XE Software, Catalyst L3 Switch Software (CAT3K_CAA-UNIVERSALK9-M), Version 03.03.02SE RELEASE SOFTWARE (fc2)
+      # hat nicht mehr zu bieten als eine einzige oid
+      # cempMemBufferNotifyEnabled .1.3.6.1.4.1.9.9.221.1.2.1.0 = INTEGER: 2
+      # deshalb:
+      $self->analyze_and_check_mem_subsystem("Classes::Cisco::CISCOMEMORYPOOLMIB::Component::MemSubsystem");
+    }
   } else {
     $self->analyze_and_check_mem_subsystem("Classes::Cisco::CISCOMEMORYPOOLMIB::Component::MemSubsystem");
   }
