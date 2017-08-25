3410a784c7698493ec60fec24b64b877d117ac04
Gerhard Lausser <gerhard.lausser@consol.de>
2017-08-25 20:27:37 +0200

    checkpoint vpn

--- plugins-scripts/Classes/CheckPoint/Firewall1/Component/VpnSubsystem.pm
+++ plugins-scripts/Classes/CheckPoint/Firewall1/Component/VpnSubsystem.pm
@@ -5,16 +5,14 @@ use strict;
 sub init {
   my $self = shift;
   $self->get_snmp_tables('CHECKPOINT-MIB', [
-      ['tunnels', 'tunnelTable', 'Classes::CheckPoint::Firewall1::Component::VpnSubsystem::Tunnel'],
-      ['permanenttunnels', 'permanentTunnelTable', 'Classes::CheckPoint::Firewall1::Component::VpnSubsystem::PermanentTunnel'],
-	      #  sub { my $o = shift; $o->{parent} = $self; $self->filter_name($o->{cikeTunRemoteValue})}],
+      ['tunnels', 'tunnelTable', 'Classes::CheckPoint::Firewall1::Component::VpnSubsystem::Tunnel', sub { my $o = shift; $o->filter_name($o->{tunnelPeerIpAddr}) || $o->filter_name($o->{tunnelPeerObjName}) } ],
+      ['permanenttunnels', 'permanentTunnelTable', 'Classes::CheckPoint::Firewall1::Component::VpnSubsystem::PermanentTunnel, sub { my $o = shift; $o->filter_name($o->{permanentTunnelPeerIpAddr}) || $o->filter_name($o->{permanentTunnelPeerObjName}) }'],
   ]);
 }
 
 sub check {
   my $self = shift;
-  return;
-  if (! @{$self->{tunnels}} || ! @{$self->{permanenttunnels}}) {
+  if (! @{$self->{tunnels}} && ! @{$self->{permanenttunnels}}) {
     $self->add_ok('no tunnels configured');
   } else {
     $self->SUPER::check();
@@ -26,11 +24,17 @@ package Classes::CheckPoint::Firewall1::Component::VpnSubsystem::Tunnel;
 our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
 use strict;
 
+sub finish {
+  my $self = shift;
+  $self->{flat_indices} =~ /^(\d+\.\d+\.\d+\.\d+)/;
+  $self->{tunnelPeerIpAddr} ||= $1;
+  $self->{tunnelPeerObjName} ||= $self->{tunnelPeerIpAddr};
+}
+
 sub check {
   my $self = shift;
-  my $name = $self->{tunnelPeerObjName} || $self->{tunnelPeerIpAddr};
   $self->add_info(sprintf 'tunnel to %s is %s',
-      $name, $self->{tunnelState});
+      $self->{tunnelPeerObjName}, $self->{tunnelState});
   if ($self->{tunnelState} =~ /^(destroy|down)$/) {
     $self->add_critical();
   } else {
@@ -39,14 +43,20 @@ sub check {
 }
 
 package Classes::CheckPoint::Firewall1::Component::VpnSubsystem::PermanentTunnel;
-our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
+our @ISA = qw(Classes::CheckPoint::Firewall1::Component::VpnSubsystem::Tunnel);
 use strict;
+
+sub finish {
+  my $self = shift;
+  $self->{flat_indices} =~ /^(\d+\.\d+\.\d+\.\d+)/;
+  $self->{permanentTunnelPeerIpAddr} ||= $1;
+  $self->{permanentTunnelPeerObjName} ||= $self->{permanentTunnelPeerIpAddr};
+}
 
 sub check {
   my $self = shift;
-  my $name = $self->{permanentTunnelPeerObjName} || $self->{permanentTunnelPeerIpAddr};
   $self->add_info(sprintf 'permanent tunnel to %s is %s',
-      $name, $self->{permanentTunnelState});
+      $self->{permanentTunnelPeerObjName}, $self->{permanentTunnelState});
   if ($self->{permanentTunnelState} =~ /^(destroy|down)$/) {
     $self->add_critical();
   } else {
