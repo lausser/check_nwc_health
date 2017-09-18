476fceb4617c47ea3f28f8273bc2fcda5e456d65
Gerhard Lausser <gerhard.lausser@consol.de>
2017-09-18 20:19:55 +0200

    blacklist l3 routing card (and power supply) if a nexus is used as l2 switch

--- plugins-scripts/Classes/Cisco/NXOS/Component/EnvironmentalSubsystem.pm
+++ plugins-scripts/Classes/Cisco/NXOS/Component/EnvironmentalSubsystem.pm
@@ -10,6 +10,24 @@ sub init {
       Classes::Cisco::CISCOENTITYSENSORMIB::Component::SensorSubsystem->new();
   if ($self->implements_mib('CISCO-ENTITY-FRU-CONTROL-MIB')) {
     $self->{fru_subsystem} = Classes::Cisco::CISCOENTITYFRUCONTROLMIB::Component::EnvironmentalSubsystem->new();
+    $self->check_l2_l3();
+    foreach my $mod (@{$self->{fru_subsystem}->{module_subsystem}->{modules}}) {
+      if (exists $mod->{entity} &&
+          $mod->{entity}->{entPhysicalDescr} =~ /L3 DAUGHTER CARD/ &&
+          $mod->get_variable('layer', 'l3') eq 'l2') {
+        # l3 routing cards may look failed, but the real cause is that the
+        # nexus is used as a l2 switch without any routing functionality.
+        $mod->blacklist();
+        #$self->annotate_info('no l3 routing');
+        foreach my $ps (@{$self->{fru_subsystem}->{powersupply_subsystem}->{powersupplies}}) {
+          # blacklist also the corresponding power supply which can look like
+          # admin status is on, oper status is offDenied
+          if ($mod->{flat_indices} eq $ps->{flat_indices}) {
+            $ps->blacklist();
+          }
+        }
+      }
+    }
   }
 }
 
@@ -31,5 +49,27 @@ sub dump {
   if (exists $self->{fru_subsystem}) {
     $self->{fru_subsystem}->dump();
   }
+}
+
+sub check_l2_l3 {
+  my $self = shift;
+  my @unrealistic_number_of_routes = ();
+  for my $masklen (1..12) {
+    push(@unrealistic_number_of_routes, 2 ** (32 - $masklen));
+  }
+  # find out if this device is L2-only (and blacklist offline L3-cpu)
+  # ipCidrRouteNumber deprecates ipForwardNumber
+  # inetCidrRouteNumber deprecates ipCidrRouteNumber
+  my $inetCidrRouteNumber =
+      $self->get_snmp_object('IP-FORWARD-MIB', 'inetCidrRouteNumber');
+  my $ipCidrRouteNumber =
+      $self->get_snmp_object('IP-FORWARD-MIB', 'ipCidrRouteNumber');
+  my $ipForwardNumber =
+      $self->get_snmp_object('IP-FORWARD-MIB', 'ipForwardNumber');
+  my $num_routes = defined $inetCidrRouteNumber ? $inetCidrRouteNumber :
+      defined $ipCidrRouteNumber ? $ipCidrRouteNumber :
+      defined $ipForwardNumber ? $ipForwardNumber : 0;
+  $num_routes = 0 if grep $num_routes, @unrealistic_number_of_routes;
+  $self->set_variable("layer", $num_routes ? "l3" : "l2");
 }
 
