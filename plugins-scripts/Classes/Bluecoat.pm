4b5de8a20b41ac11d6a6f05c8a04fef26fb42fe9
Gerhard Lausser <gerhard.lausser@consol.de>
2018-01-23 10:50:19 +0100

    fix bluecoat asg detection

--- plugins-scripts/Classes/Bluecoat.pm
+++ plugins-scripts/Classes/Bluecoat.pm
@@ -5,7 +5,7 @@ use strict;
 
 sub init {
   my ($self) = @_;
-  if ($self->{productname} =~ /Blue.*Coat.*(SG\d+|SGOS)/i) {
+  if ($self->{productname} =~ /Blue.*Coat.*(SG\d+|SGOS|ASG)/i) {
     # product ProxySG  Blue Coat SG600
     # iso.3.6.1.4.1.3417.2.11.1.3.0 = STRING: "Version: SGOS 5.5.8.1, Release id: 78642 Proxy Edition"
     bless $self, 'Classes::SGOS';
