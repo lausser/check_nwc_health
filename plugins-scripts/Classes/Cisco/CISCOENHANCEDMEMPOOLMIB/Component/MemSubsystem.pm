cb426524fe95206e4151078a24bfb25fd59e8569
Gerhard Lausser <gerhard.lausser@consol.de>
2018-05-05 16:39:12 +0200

    simplify the cisco memory exceptions

--- plugins-scripts/Classes/Cisco/CISCOENHANCEDMEMPOOLMIB/Component/MemSubsystem.pm
+++ plugins-scripts/Classes/Cisco/CISCOENHANCEDMEMPOOLMIB/Component/MemSubsystem.pm
@@ -34,39 +34,30 @@ sub check {
   my ($self) = @_;
   $self->add_info(sprintf 'mempool %s usage is %.2f%%',
       $self->{name}, $self->{usage});
+  my $used100 = 0;
   if ($self->{name} =~ /^lsmpi_io/ &&
       $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /IOS.*(XE|ASR1000)/i) {
     # https://supportforums.cisco.com/docs/DOC-16425
-    $self->force_thresholds(
-        metric => $self->{name}.'_usage',
-        warning => 100,
-        critical => 100,
-    );
-  } elsif ($self->{name} =~ /global.*shared/i) {
-    if ($self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0)
-        =~ /(asa|adaptive security appliance)/i) {
-      if ($self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /Version ([\d\.]+)/) {
-        $self->set_variable("version", $1);
-        if ($self->version_is_minimum("9.3.2")) {
-          # Cisco Adaptive Security Appliance Version 9.4(4)16
-          $self->force_thresholds(
-              metric => $self->{name}.'_usage',
-              warning => 100,
-              critical => 100,
-          );
-        }
+    $used100 = 1;
+  } elsif ($self->{name} =~ /global.*shared/i &&
+      $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0)
+          =~ /(asa|adaptive security appliance)/i) {
+    if ($self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /Version ([\d\.]+)/) {
+      $self->set_variable("version", $1);
+      if ($self->version_is_minimum("9.3.2")) {
+        # Cisco Adaptive Security Appliance Version 9.4(4)16
+        $used100 = 1;
       }
     }
   } elsif ($self->{name} =~ /^reserved/ &&
       $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /IOS.*XR/i) {
     # ASR9K "reserved" and "image" are always at 100%
-    $self->force_thresholds(
-        metric => $self->{name}.'_usage',
-        warning => 100,
-        critical => 100,
-    );
+    $used100 = 1;
   } elsif ($self->{name} =~ /^image/ &&
       $self->get_snmp_object('MIB-2-MIB', 'sysDescr', 0) =~ /IOS.*XR/i) {
+    $used100 = 1;
+  }
+  if ($used100) {
     $self->force_thresholds(
         metric => $self->{name}.'_usage',
         warning => 100,
