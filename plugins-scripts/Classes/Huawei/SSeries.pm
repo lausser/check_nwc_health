28c79bd237e899d0ba048d61677fd2064208e66e
Rika Lena Denia <rika.denia@comnetgmbh.com>
2018-09-28 17:28:59 +0200

    Huawei: Reroute cpu and memory checks to dedicated S series packages

--- plugins-scripts/Classes/Huawei/SSeries.pm
+++ plugins-scripts/Classes/Huawei/SSeries.pm
@@ -8,9 +8,9 @@ sub init {
   if ($self->mode =~ /device::hardware::health/) {
     $self->analyze_and_check_environmental_subsystem("Classes::Huawei::Component::EnvironmentalSubsystem");
   } elsif ($self->mode =~ /device::hardware::load/) {
-    $self->analyze_and_check_cpu_subsystem("Classes::Huawei::Component::CpuSubsystem");
+    $self->analyze_and_check_cpu_subsystem("Classes::Huawei::SSeries::CpuSubsystem");
   } elsif ($self->mode =~ /device::hardware::memory/) {
-    $self->analyze_and_check_mem_subsystem("Classes::Huawei::Component::MemSubsystem");
+    $self->analyze_and_check_mem_subsystem("Classes::Huawei::SSeries::MemSubsystem");
   } else {
     $self->no_such_mode();
   }
