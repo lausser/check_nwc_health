1a8fb43c732f7c9f1f3fa2a341dfd1044c2343ee
Gerhard Lausser <gerhard.lausser@consol.de>
2019-02-18 14:55:28 +0100

    add Draytek Vigor

---
+++ plugins-scripts/Classes/DrayTek/Vigor/Component/EnvironmentalSubsystem.pm
@@ -0,0 +1,34 @@
+package Classes::DrayTek::Vigor::Component::EnvironmentalSubsystem;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::Item);
+use strict;
+use Data::Dumper;
+
+sub init {
+  my ($self) = @_;
+  $self->get_snmp_tables('ADSL-LINE-MIB', [
+      ['lines', 'adslAturPhysTable', 'Classes::DrayTek::Vigor::Component::AdslLine'],
+  ]);
+}
+
+
+package Classes::DrayTek::Vigor::Component::AdslLine;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
+use strict;
+
+sub finish {
+  my ($self) = @_;
+  chomp $self->{adslAturCurrStatus};
+  $self->{adslAturCurrStatus} =~ s/\x0+$//;
+}
+
+sub check {
+  my ($self) = @_;
+  $self->add_info(sprintf 'adsl line %s has status _%s_', 
+      $self->{flat_indices}, $self->{adslAturCurrStatus});
+  if ($self->{adslAturCurrStatus} ne "SHOWTIME") {
+    $self->add_critical();
+  } else {
+    $self->add_ok();
+  }
+}
+
