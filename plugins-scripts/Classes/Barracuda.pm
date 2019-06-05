c065a29be21c9f50d0dd3ba9389dcd27259904f7
Gerhard Lausser <gerhard.lausser@consol.de>
2019-06-05 21:30:32 +0200

    add barracuda files (not finished yet)

--- plugins-scripts/Classes/Barracuda.pm
+++ plugins-scripts/Classes/Barracuda.pm
@@ -11,8 +11,14 @@ sub init {
     $self->analyze_and_check_cpu_subsystem("Classes::UCDMIB::Component::CpuSubsystem");
   } elsif ($self->mode =~ /device::hardware::memory/) {
     $self->analyze_and_check_mem_subsystem("Classes::UCDMIB::Component::MemSubsystem");
+  } elsif ($self->mode =~ /device::fw::policy::connections/) {
+    $self->analyze_and_check_fw_subsystem("Classes::Barracuda::Component::FwSubsystem");
+  } elsif ($self->mode =~ /device::ha::/) {
+    $self->analyze_and_check_fw_subsystem("Classes::Barracuda::Component::HaSubsystem");
   } else {
+    # Merkwuerdigerweise gibts ohne das hier einen Timeout bei
+    # IP-FORWARD-MIB::inetCidrRouteTable und den route-Modi
+    $self->mult_snmp_max_msg_size(5);
     $self->no_such_mode();
   }
 }
-
