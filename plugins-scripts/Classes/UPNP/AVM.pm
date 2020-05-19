92804b03bb93cbcc8764cf4ad79dfe028e3293c6
Gerhard Lausser <gerhard.lausser@consol.de>
2020-05-19 11:01:28 +0200

    fix a typo

--- plugins-scripts/Classes/UPNP/AVM.pm
+++ plugins-scripts/Classes/UPNP/AVM.pm
@@ -5,7 +5,7 @@ use strict;
 sub init {
   my ($self) = @_;
   if ($self->{productname} =~ /(7390|7490|7580|7590|6490|7412)/) {
-    $self->rebless('Classes::UPNP::AVM::FritzBox7390'i);
+    $self->rebless('Classes::UPNP::AVM::FritzBox7390');
   } else {
     $self->no_such_model();
   }
