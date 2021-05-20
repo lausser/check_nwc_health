37eaf73f4fbbfae11035d492dbfdc3d1c84b4fa3
Christopher Kreft <email@christopherkreft.de>
2021-05-20 13:12:04 +0200

    feature fortigate vpn sessions

---
+++ plugins-scripts/Classes/Fortigate/Component/VpnSubsystem.pm
@@ -0,0 +1,24 @@
+package Classes::Fortigate::Component::VpnSubsystem;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::Item);
+use strict;
+
+sub init {
+  my ($self, %params) = @_;
+  my $type = 0;
+  $self->get_snmp_objects('FORTINET-FORTIGATE-MIB', (qw(
+      fgSysSesCount)));
+}
+
+sub check {
+  my ($self) = @_;
+  my $errorfound = 0;
+  $self->add_info('checking vpn sessions');
+  $self->add_info(sprintf '%u vpn sessions', $self->{fgSysSesCount});
+  $self->set_thresholds(warning => 25000, critical => 50000);
+  $self->add_message($self->check_thresholds($self->{fgSysSesCount}));
+  $self->add_perfdata(
+      label => 'vpn_session_count',
+      value => $self->{fgSysSesCount},
+  );
+}
+
