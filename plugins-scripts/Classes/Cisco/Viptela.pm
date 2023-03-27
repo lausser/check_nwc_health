74352aa8dbf84acc4d0bb252e1eaabfffaccb8d1
Gerhard Lausser <gerhard.lausser@consol.de>
2023-03-27 14:55:55 +0200

    remove sdwan from viptela class

--- plugins-scripts/Classes/Cisco/Viptela.pm
+++ plugins-scripts/Classes/Cisco/Viptela.pm
@@ -10,17 +10,6 @@ sub init {
     $self->analyze_and_check_cpu_subsystem("Classes::Cisco::Viptela::Component::CpuSubsystem");
   } elsif ($self->mode =~ /device::hardware::memory/) {
     $self->analyze_and_check_mem_subsystem("Classes::Cisco::Viptela::Component::MemSubsystem");
-  } elsif ($self->mode =~ /device::sdwan::/) {
-  #} elsif ($self->mode =~ /device::sdwan::session::availability/) {
-    $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'CISCO-VIPTELA-MIB'} = {};
-    $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'CISCO-VIPTELA-MIB'}->{configuredConnections} = '1.3.6.1.4.1.9.9.1002.1.1.5.1';
-    $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'CISCO-VIPTELA-MIB'}->{activeConnections} = '1.3.6.1.4.1.9.9.1002.1.1.5.2';
-    $self->get_snmp_objects("CISCO-VIPTELA-MIB", qw(configuredConnections activeConnections));
-    if (defined $self->{configuredConnections}) {
-      $self->analyze_and_check_sdwan_subsystem("Classes::Cisco::Viptela::Component::SdwanSubsystem");
-    } else {
-      $self->no_such_mode();
-    }
   } else {
     $self->no_such_mode();
   }
