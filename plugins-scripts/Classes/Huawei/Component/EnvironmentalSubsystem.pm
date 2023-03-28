9b618430ebcd70f1b46745d85c70b805f019fbd8
Gerhard Lausser <gerhard.lausser@consol.de>
2023-03-28 16:41:37 +0200

    improve Huawei hardware-health

--- plugins-scripts/Classes/Huawei/Component/EnvironmentalSubsystem.pm
+++ plugins-scripts/Classes/Huawei/Component/EnvironmentalSubsystem.pm
@@ -22,35 +22,71 @@ sub init {
   # snmp responses when a large amount of data was transmitted.
   $self->mult_snmp_max_msg_size(20);
   $self->bulk_is_baeh(30);
-  $self->get_snmp_tables('HUAWEI-ENTITY-EXTENT-MIB', [
-      ['entitystates', 'hwEntityStateTable',
-      'Monitoring::GLPlugin::SNMP::TableItem'],
-  ]);
-  $self->debug(sprintf "found %d %s", scalar(@{$self->{entitystates}}), "entitystates");
   foreach (qw(modules fans powersupplies)) {
+    # we need to get the table inside the loop, as merge_table deletes
+    # entitystates. get_snmp_tables will read from the cache.
+    $self->get_snmp_tables('HUAWEI-ENTITY-EXTENT-MIB', [
+        ['entitystates', 'hwEntityStateTable',
+        'Monitoring::GLPlugin::SNMP::TableItem'],
+    ]);
+    $self->debug(sprintf "found %d %s", scalar(@{$self->{entitystates}}), "entitystates");
     $self->debug(sprintf "found %d %s", scalar(@{$self->{$_}}), $_);
     $self->merge_tables($_, "entitystates");
   }
   $self->get_snmp_tables('HUAWEI-ENTITY-EXTENT-MIB', [
-      ['fanstates', 'hwFanStatusTable', 'Monitoring::GLPlugin::SNMP::TableItem'],
+      ['fanstates', 'hwFanStatusTable', 'Classes::Huawei::Component::EnvironmentalSubsystem::FanStatus']
   ]);
   $self->debug(sprintf "found %d %s", scalar(@{$self->{fanstates}}), "fanstates");
   if (@{$self->{fanstates}} && ! @{$self->{fans}}) {
     # gibts auch, d.h. retten, was zu retten ist
-    foreach (@{$self->{fanstates}}) {
-      bless $_, "Classes::Huawei::Component::EnvironmentalSubsystem::Fan";
-      $_->{entPhysicalName} = $_->{flat_indices};
-      $_->finish();
-    }
+    @{$self->{fanstates}} = grep {
+      $_->{hwEntityFanPresent} eq "present";
+    } @{$self->{fanstates}};
   } else {
     $self->merge_tables_with_code("fans", "fanstates", sub {
       my ($fan, $fanstate) = @_;
       return ($fan->{entPhysicalName} eq sprintf("FAN %d/%d",
           $fanstate->{hwEntityFanSlot}, $fanstate->{hwEntityFanSn})) ? 1 : 0;
     });
+    if (grep { exists $_->{hwEntityFanState} } @{$self->{fans}}) {
+      # fans and fanstates matched, check fans
+    } else {
+      # $fan->{entPhysicalName} and $fanstate->{Slot/Sn} were different
+      # there was also a device with 4 fans and 8 fanstates. Dreck!
+      # better check fanstates
+      $self->get_snmp_tables('HUAWEI-ENTITY-EXTENT-MIB', [
+          ['fanstates', 'hwFanStatusTable', 'Classes::Huawei::Component::EnvironmentalSubsystem::FanStatus']
+      ]);
+      delete $self->{fans};
+    }
   }
 }
 
+
+package Classes::Huawei::Component::EnvironmentalSubsystem::FanStatus;
+our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
+use strict;
+
+sub finish {
+  my ($self) = @_;
+  $self->{name} = $self->{hwEntityFanDesc} || sprintf("FAN %d/%d",
+      $self->{hwEntityFanSlot}, $self->{hwEntityFanSn});
+}
+
+sub check {
+  my ($self) = @_;
+  $self->add_info(sprintf 'fan %s state is %s',
+      $self->{name},
+      $self->{hwEntityFanState});
+  if ($self->{hwEntityFanState} ne 'normal') {
+    $self->add_warning();
+  }
+  $self->add_perfdata(
+      label => 'rpm_'.$self->{name},
+      value => $self->{hwEntityFanSpeed},
+      uom => '%',
+  );
+}
 
 package Classes::Huawei::Component::EnvironmentalSubsystem::Fan;
 our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
@@ -81,8 +117,79 @@ sub check {
   }
 }
 
-package Classes::Huawei::Component::EnvironmentalSubsystem::Powersupply;
+package Classes::Huawei::Component::EnvironmentalSubsystem::Entity;
 our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
+use strict;
+
+sub check {
+  my ($self) = @_;
+  if ($self->{hwEntityOperStatus} eq 'down' ||
+      $self->{hwEntityOperStatus} eq 'disabled' ||
+      $self->{hwEntityOperStatus} eq 'offline') {
+    # disabled is the important one
+    # A value of disabled means the resource is totally
+    #      inoperable. A value of enabled means the resource
+    #      is partially or fully operableA"
+    $self->add_warning();
+  }
+  if ($self->{hwEntityTemperature}) {
+    # Es gibt viele module POWER Card 0/PWR1 temperature is 0.00
+    # Selbst am Nordpol duerfte so ein Ding waermer als 0 Grad sein, also
+    # gehe ich davon aus, daß da kein Sensor verbaut ist.
+    $self->add_info(sprintf 'module %s temperature is %.2f',
+        $self->{entPhysicalName}, $self->{hwEntityTemperature});
+    $self->set_thresholds(
+        metric => 'temp_'.$self->{entPhysicalName},
+        warning => $self->{hwEntityTemperatureLowThreshold}.':'.$self->{hwEntityTemperatureThreshold},
+        critical => $self->{hwEntityTemperatureLowThreshold}.':'.$self->{hwEntityTemperatureThreshold},
+    );
+    my $level = $self->check_thresholds(
+        metric => 'temp_'.$self->{entPhysicalName},
+        value => $self->{hwEntityTemperature});
+    $self->add_message($level);
+    $self->add_perfdata(
+        label => 'temp_'.$self->{entPhysicalName},
+        value => $self->{hwEntityTemperature},
+    );
+  }
+  if ($self->{hwEntityAlarmLight}) {
+    my @alarms = grep {
+      # Hab nachschauen lassen bei einem fetten Router, der bei allen Modulen
+      # und Powersupplies "alarm light status is indeterminate" angezeigt hat.
+      # "die Kiste sieht in Ordnung aus"
+      # Also fliegt das raus, denn beim ersten Alarm wuerde es sowieso wieder
+      # heissen "mimimi, kann man das nicht clientseitig abfangen?"
+      $_ ne "indeterminate";
+    } grep {
+      # Den auch nochmal putzen
+      $_ ne "notSupported";
+    } split(",", $self->{hwEntityAlarmLight});
+    $self->annotate_info("alarm light status is ".join("+", @alarms))
+        if (@alarms);
+    foreach my $alarm (@alarms) {
+      if ($alarm eq "underRepair" or $alarm eq "minor" or $alarm eq "warning") {
+        $self->add_warning($alarm." alarm at ".$self->{entPhysicalName});
+      } elsif ($alarm eq "critical" or $alarm eq "major") {
+        $self->add_critical($alarm." alarm at ".$self->{entPhysicalName});
+      } elsif ($alarm eq "alarmOutstanding") {
+        # When the value of alarm outstanding is set, one or more
+        # alarms is active against the resource. The fault may or may
+        # not be disabling.
+        # Kapier ich nicht ganz. Also erstmal Alarm, bis sich einer beschwert.
+        $self->add_critical($alarm." alarm at ".$self->{entPhysicalName});
+      }
+    }
+  }
+  if ($self->{hwEntityFaultLight} and not $self->{hwEntityFaultLight} eq "notSupported") {
+    # The repair status for this entity
+    $self->annotate_info(sprintf 'fault light is %s',
+        $self->{hwEntityFaultLight});
+  }
+}
+
+
+package Classes::Huawei::Component::EnvironmentalSubsystem::Powersupply;
+our @ISA = qw(Classes::Huawei::Component::EnvironmentalSubsystem::Entity);
 use strict;
 
 sub finish {
@@ -92,46 +199,27 @@ sub finish {
 
 sub check {
   my ($self) = @_;
-  $self->add_info(sprintf 'powersupply %s has admin status is %s, oper status is %s',
+  $self->add_info(sprintf 'powersupply %s admin status is %s, oper status is %s',
       $self->{entPhysicalName},
       $self->{hwEntityAdminStatus}, $self->{hwEntityOperStatus});
-  if ($self->{hwEntityOperStatus} eq 'down' ||
-      $self->{hwEntityOperStatus} eq 'offline') {
-    $self->add_warning();
-  }
+  $self->SUPER::check();
 }
 
 package Classes::Huawei::Component::EnvironmentalSubsystem::Module;
-our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
+our @ISA = qw(Classes::Huawei::Component::EnvironmentalSubsystem::Entity);
 use strict;
 
 sub finish {
   my ($self) = @_;
-  $self->{name} = $self->{entPhysicalName};
+  $self->{hwEntityAlarmLight} = "notSupported" if ! defined $self->{hwEntityAlarmLight};
 }
 
 sub check {
   my ($self) = @_;
   $self->add_info(sprintf 'module %s admin status is %s, oper status is %s',
-      $self->{name}, $self->{hwEntityAdminStatus}, $self->{hwEntityOperStatus});
-  $self->add_info(sprintf 'module %s temperature is %.2f',
-      $self->{name}, $self->{hwEntityTemperature});
-  $self->set_thresholds(
-      metric => 'temp_'.$self->{name},
-      warning => $self->{hwEntityTemperatureLowThreshold}.':'.$self->{hwEntityTemperatureThreshold},
-      critical => $self->{hwEntityTemperatureLowThreshold}.':'.$self->{hwEntityTemperatureThreshold},
-  );
-  $self->add_message(
-      $self->check_thresholds(
-          metric => 'temp_'.$self->{name},
-          value => $self->{hwEntityTemperature}
-  ));
-  $self->add_perfdata(
-      label => 'temp_'.$self->{name},
-      value => $self->{hwEntityTemperature},
-  );
-  $self->add_info(sprintf 'module %s fault light is %s',
-      $self->{name}, $self->{hwEntityFaultLight}) if exists $self->{hwEntityFaultLight};
+      $self->{entPhysicalName},
+      $self->{hwEntityAdminStatus}, $self->{hwEntityOperStatus});
+  $self->SUPER::check();
 }
 
 
@@ -168,4 +256,35 @@ hwEntityTemperature: 33
 hwEntityTemperatureLowThreshold: 0
 hwEntityTemperatureThreshold: 62
 hwEntityUpTime: 34295804
+
+# sowas gibts auch, da ist die Stromversorgung per module, nicht powerSupply
+# verbaut.
+[MODULE_67190797]
+entPhysicalClass: module
+entPhysicalDescr: POWER Card
+entPhysicalName: POWER Card 0/PWR1
+hwEntityAdminStatus: unlocked
+hwEntityAlarmLight: unknown_
+hwEntityBoardName: POWER
+hwEntityCpuMaxUsage: 0
+hwEntityCpuUsage: 0
+hwEntityCpuUsageLowThreshold: 0
+hwEntityCpuUsageThreshold: 0
+hwEntityFaultLight: notSupported
+hwEntityFaultLightKeepTime: 0
+hwEntityMemSize: 0
+hwEntityMemUsage: 0
+hwEntityMemUsageThreshold: 0
+hwEntityOperStatus: enabled
+hwEntitySplitAttribute: 
+hwEntityStandbyStatus: notSupported
+hwEntityTemperature: 0
+hwEntityTemperatureLowThreshold: 0
+hwEntityTemperatureThreshold: 0
+hwEntityUpTime: 0
+hwEntityVoltage: 0
+hwEntityVoltageHighThreshold: 0
+hwEntityVoltageLowThreshold: 0
+
+
 
