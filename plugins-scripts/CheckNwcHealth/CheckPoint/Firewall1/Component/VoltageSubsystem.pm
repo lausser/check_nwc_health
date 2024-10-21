6364b7822a7fed143d4675bfa53a4bc3b1616c31
Gerhard Lausser <gerhard.lausser@consol.de>
2024-10-21 14:52:51 +0200

    repaired PR #335 (which was the last PR i will ever read)

--- plugins-scripts/CheckNwcHealth/CheckPoint/Firewall1/Component/VoltageSubsystem.pm
+++ plugins-scripts/CheckNwcHealth/CheckPoint/Firewall1/Component/VoltageSubsystem.pm
@@ -1,7 +1,6 @@
 package CheckNwcHealth::CheckPoint::Firewall1::Component::VoltageSubsystem;
 our @ISA = qw(Monitoring::GLPlugin::SNMP::Item);
 use strict;
-use Scalar::Util qw(looks_like_number);
 
 sub init {
   my ($self) = @_;
@@ -21,36 +20,36 @@ sub check {
 package CheckNwcHealth::CheckPoint::Firewall1::Component::VoltageSubsystem::Voltage;
 our @ISA = qw(Monitoring::GLPlugin::SNMP::TableItem);
 use strict;
+use Scalar::Util qw(looks_like_number);
 
 sub check {
   my ($self) = @_;
   if ($self->{voltageSensorValue} && looks_like_number($self->{voltageSensorValue})) {
-	  $self->add_info(sprintf 'voltage %s is %s (%.2f %s)', 
-		  $self->{voltageSensorName}, $self->{voltageSensorStatus},
-		  $self->{voltageSensorValue}, $self->{voltageSensorUnit});
-	  if ($self->{voltageSensorStatus} eq 'normal') {
-		$self->add_ok();
-	  } elsif ($self->{voltageSensorStatus} eq 'abnormal') {
-		$self->add_critical();
-	  } else {
-		$self->add_unknown();
-	  }
-	  $self->set_thresholds(warning => 60, critical => 70);
-	  $self->add_perfdata(
-		  label => 'voltage'.$self->{voltageSensorName}.'_rpm',
-		  value => $self->{voltageSensorValue},
-	  );
+    $self->add_info(sprintf 'voltage %s is %s (%.2f %s)',
+        $self->{voltageSensorName}, $self->{voltageSensorStatus},
+        $self->{voltageSensorValue}, $self->{voltageSensorUnit});
+    if ($self->{voltageSensorStatus} eq 'normal') {
+      $self->add_ok();
+    } elsif ($self->{voltageSensorStatus} eq 'abnormal') {
+      $self->add_critical();
+    } else {
+      $self->add_unknown();
+    }
+    $self->add_perfdata(
+        label => 'voltage'.$self->{voltageSensorName},
+        value => $self->{voltageSensorValue},
+    );
   } else {
-	$self->add_info(sprintf 'voltage %s is %s (%s %s)', 
-		  $self->{voltageSensorName}, $self->{voltageSensorStatus},
-		  $self->{voltageSensorValue}, $self->{voltageSensorUnit});
-	  if ($self->{voltageSensorStatus} eq 'normal') {
-		$self->add_ok();
-	  } elsif ($self->{voltageSensorStatus} eq 'abnormal') {
-		$self->add_critical();
-	  } else {
-		$self->add_unknown();
-	  }
+    $self->add_info(sprintf 'voltage %s is %s (%s %s)',
+        $self->{voltageSensorName}, $self->{voltageSensorStatus},
+        $self->{voltageSensorValue}, $self->{voltageSensorUnit});
+    if ($self->{voltageSensorStatus} eq 'normal') {
+      $self->add_ok();
+    } elsif ($self->{voltageSensorStatus} eq 'abnormal') {
+      $self->add_critical();
+    } else {
+      $self->add_unknown();
+    }
   }
 }
 
